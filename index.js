fa764d4f87a61ad5a29e59c5a8eb7b99db9fb566
Darya Rosikhina <drosikhina@meetup.com>
2019-01-18 00:36:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your code in this file!
 function scuberGreetingForFeet(feet) {
-  if (feet < 401) {
-    return ('This one is on me!')
-} else if (scuberGreetingForFeet >400)
+  if (feet <= 400) {
+  return ('This one is on me!')
+} else if (feet >400) {
   return
 } 
   
