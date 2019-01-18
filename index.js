9059768492d936aa3c6ea73a4ca2e4efe4533e89
Darya Rosikhina <drosikhina@meetup.com>
2019-01-18 00:28:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  let result
+  if (distanceTraveled < 401) {
+    result = scuberGreetingForFeet(199)
+}
+
+  return result
+  
+function switchOnCharmFromTip()
+
+function ternaryCheckCity() 
