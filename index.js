f60c915581dfb37471bfe45a98a278e4a4991e82
Darya Rosikhina <drosikhina@meetup.com>
2019-01-18 00:39:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function scuberGreetingForFeet(feet) {
-  if (feet <= 400) {
+function scuberGreetingForFeet(Feet) {
+  if (Feet <= 400) {
   return ('This one is on me!')
-} else if (feet > 2000) {
+} else if (Feet > 2000) {
   return ('I will gladly take your thirty bucks.')
 } 
   
